40123a2c08253d5bbc14a8d45314ab0610e08b27
Marc <scramblelock@gmail.com>
2022-05-20 09:06:50 -0400

    meta tag in html head

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Scramblelock</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
       <HomePage />
     </>
